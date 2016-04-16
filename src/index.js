040b461baba954febc725b115fbef628f36ffdd6
Stephen Scott <me@suchipi.com>
2016-04-16 13:33:00 -0600

    Support Class Expressions too

--- src/index.js
+++ src/index.js
@@ -1,51 +1,67 @@
 import template from "babel-template";
 
 module.exports = function({ types: t }) {
-  return {
-    visitor: {
-      ClassDeclaration(path) {
-        if (!(t.isIdentifier(path.node.id) && t.isIdentifier(path.node.superClass))) {
-          return;
+  var tmpl = template(`
+    (function(SUPER_INSIDE){
+      var CHILD_INSIDE_1 = CLASS_EXPRESSION
+      if (SUPER_INSIDE.hasOwnProperty("onInherited")) {
+        if (typeof SUPER_INSIDE.onInherited == 'function') {
+          var CHILD_INSIDE_2 = SUPER_INSIDE.onInherited(CHILD_INSIDE_1) || CHILD_INSIDE_1;
+        } else {
+          throw new TypeError("Attempted to call onInherited, but it was not a function");
         }
+      }
+      return CHILD_INSIDE_2;
+    })(SUPER_OUTSIDE)
+  `);
 
-        var superClassName = path.node.superClass.name;
-        var childClassName = path.node.id.name;
+  function transform(childClassName, path) {
+      var superClassName = path.node.superClass.name;
 
-        var tmpl = template(`
-          (function(SUPER_INSIDE){
-            var CHILD_INSIDE_1 = CLASS_EXPRESSION
-            if (SUPER_INSIDE.hasOwnProperty("onInherited")) {
-              if (typeof SUPER_INSIDE.onInherited == 'function') {
-                var CHILD_INSIDE_2 = SUPER_INSIDE.onInherited(CHILD_INSIDE_1) || CHILD_INSIDE_1;
-              } else {
-                throw new TypeError("Attempted to call onInherited, but it was not a function");
-              }
-            }
-            return CHILD_INSIDE_2;
-          })(SUPER_OUTSIDE)
-        `)
+      var SUPER_INSIDE = path.scope.generateUidIdentifier(superClassName);
+      var CHILD_INSIDE_1 = path.scope.generateUidIdentifier(childClassName);
+      var CLASS_EXPRESSION = t.classExpression(
+        null,
+        SUPER_INSIDE,
+        path.node.body,
+        []
+      );
+      // Don't transform *this* class expression, or we'll loop forever
+      CLASS_EXPRESSION.__babelPluginTransformClassInheritedHook_skip = true;
+      var CHILD_INSIDE_2 = path.scope.generateUidIdentifier(childClassName);
+      var SUPER_OUTSIDE = t.identifier(superClassName);
 
-        var SUPER_INSIDE = path.scope.generateUidIdentifier(superClassName);
-        var CHILD_INSIDE_1 = path.scope.generateUidIdentifier(childClassName);
-        var CLASS_EXPRESSION = t.classExpression(
-          null,
-          SUPER_INSIDE,
-          path.node.body,
-          []
-        );
-        var CHILD_INSIDE_2 = path.scope.generateUidIdentifier(childClassName);
-        var SUPER_OUTSIDE = t.identifier(superClassName);
+      return tmpl({
+        SUPER_INSIDE,
+        CHILD_INSIDE_1,
+        CLASS_EXPRESSION,
+        CHILD_INSIDE_2,
+        SUPER_OUTSIDE
+      });
+    }
 
-        var expressionStatement = tmpl({
-          SUPER_INSIDE,
-          CHILD_INSIDE_1,
-          CLASS_EXPRESSION,
-          CHILD_INSIDE_2,
-          SUPER_OUTSIDE
-        });
+    return {
+      visitor: {
+        "ClassDeclaration|ClassExpression"(path) {
+          if (!path.node.superClass) return;
+          if (path.node.__babelPluginTransformClassInheritedHook_skip) return;
 
-        path.remove();
-        path.scope.parent.push({ id: t.identifier(childClassName), init: expressionStatement.expression });
+          var childClassName;
+
+          if (t.isIdentifier(path.node.id)) {
+            childClassName = path.node.id.name;
+          } else if (path.node.id == null && t.isVariableDeclarator(path.parentPath.node)) {
+            childClassName = path.parentPath.node.id.name;
+          }
+
+          var expressionStatement = transform(childClassName, path);
+
+          if (t.isClassDeclaration(path.node)) {
+            path.remove();
+            path.scope.parent.push({ id: t.identifier(childClassName), init: expressionStatement.expression });
+          } else if (t.isClassExpression(path.node)) {
+            path.replaceWith(expressionStatement.expression);
+          }
       }
     }
   }
---
+++ test/fixtures/NamedClassExpressionWithinVariableDeclaration/expected.js
@@ -0,0 +1,52 @@
+"use strict";
+
+var _getPrototypeOf = require("babel-runtime/core-js/object/get-prototype-of");
+
+var _getPrototypeOf2 = _interopRequireDefault(_getPrototypeOf);
+
+var _classCallCheck2 = require("babel-runtime/helpers/classCallCheck");
+
+var _classCallCheck3 = _interopRequireDefault(_classCallCheck2);
+
+var _createClass2 = require("babel-runtime/helpers/createClass");
+
+var _createClass3 = _interopRequireDefault(_createClass2);
+
+var _possibleConstructorReturn2 = require("babel-runtime/helpers/possibleConstructorReturn");
+
+var _possibleConstructorReturn3 = _interopRequireDefault(_possibleConstructorReturn2);
+
+var _inherits2 = require("babel-runtime/helpers/inherits");
+
+var _inherits3 = _interopRequireDefault(_inherits2);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var Apple = function (_Fruit) {
+  var _Banana = function (_Fruit2) {
+    (0, _inherits3.default)(_Banana, _Fruit2);
+
+    function _Banana() {
+      (0, _classCallCheck3.default)(this, _Banana);
+      return (0, _possibleConstructorReturn3.default)(this, (0, _getPrototypeOf2.default)(_Banana).apply(this, arguments));
+    }
+
+    (0, _createClass3.default)(_Banana, [{
+      key: "tastiness",
+      value: function tastiness() {
+        return 7;
+      }
+    }]);
+    return _Banana;
+  }(_Fruit);
+
+  if (_Fruit.hasOwnProperty("onInherited")) {
+    if (typeof _Fruit.onInherited == 'function') {
+      var _Banana2 = _Fruit.onInherited(_Banana) || _Banana;
+    } else {
+      throw new TypeError("Attempted to call onInherited, but it was not a function");
+    }
+  }
+
+  return _Banana2;
+}(Fruit);
